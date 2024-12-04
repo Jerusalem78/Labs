ce58813ebfdf1b96200d77426c47bd5869896c08
root <root@DESKTOP-S575TBQ.>
2024-12-04 21:50:44 +0300

    eighth

---
+++ src/lab9.sh
@@ -0,0 +1,10 @@
+sudo apt-get install git
+git clone https://github.com/Jerusalem78/Labs
+cd ~/Labs/src
+touch lab9.sh
+echo sudo apt-get install git
+git clone https://github.com/Jerusalem78/Labs
+cd ~/Labs/src
+touch lab9.sh
+git add lab9.sh
+git commit -m 'eighth'
