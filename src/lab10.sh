24236f834b0b527c2db14688e04b5255040c46be
Jerusalem78 <denisfomin06@gmail.com>
2025-02-08 15:10:03 +0300

    ninth

---
+++ src/lab10.sh
@@ -0,0 +1,8 @@
+git clone https://github.com/Jerusalem78/Labs
+# git remote add https://github.com/Jerusalem78/Labs
+cd \Labs
+git push origin main
+git checkout -b dev
+git push origin dev
+git push origin --delete dev
+git pull origin main
